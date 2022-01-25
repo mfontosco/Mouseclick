71c24e48f39c0d1e1a25708bf4fb3f6c9e7a225d
DESKTOP-568P3FP\Divine favour <mstrings11@gmail.com>
2022-01-25 11:20:43 +0100

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    ///private SceneManager sceneManager;
+    public List<GameObject> prefabs;
+    private float spawnRate = 1f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    public Button restartBtn;
+    public bool isGameActive;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        titleScreen.gameObject.SetActive(false);
+        isGameActive = true;
+        score = 0;
+        StartCoroutine(SpawnTargets());
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateScore(int ScoreToAdd)
+    {
+        score += ScoreToAdd;
+        scoreText.text = "Score:" + score;
+    }
+
+    IEnumerator SpawnTargets()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, prefabs.Count);
+            Instantiate(prefabs[index]);
+            
+        }
+        
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartBtn.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        titleScreen.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    
+    [SerializeField] private float speed;
+    [SerializeField] private float maximumHeight = 16;
+    [SerializeField] private float minimumHeight = 12;
+    public GameManager gameManager;
+    public ParticleSystem explosionParticle;
+    private Rigidbody playerRb;
+    private float maxTorque = 10;
+    private float minTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = 6;
+    public int pointValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(RandomForce() * speed, ForceMode.Impulse);
+        playerRb.AddTorque(RandomTorque(),RandomTorque(),RandomTorque());
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        gameManager.GameOver();
+        Destroy(gameObject);
+    }
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minimumHeight,maximumHeight);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-minTorque,maxTorque);
+    }
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange,xRange), ySpawnPos);
+    }
+    //private void FixedUpdate()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        playerRb.AddForce(Vector3.up * Random.Range(12, 16) * speed, ForceMode.Impulse);
+    //    }
+       
+    //}
+}
